a86ac293f39a619a8f41c8bd7e14e8e60d541ef6
souparno24 <sou24par08no1999@gmail.com>
2022-03-14 12:36:33 +0530

    layout plus validation

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'users/new'
   root 'static_pages#home'
+  get  '/help' => 'static_pages#help'
+  get  '/about' =>  'static_pages#about'
+  get  '/contact' =>  'static_pages#contact'
+  get  '/signup' => 'users#new'
+  
 end
